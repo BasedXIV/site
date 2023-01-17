81e1737387d50dce572eaf2267b1eeb76697bcbc
Terry Wang <terry@tianyu.wang>
2023-01-16 21:21:20 -0500

    Updates

---
+++ precache-manifest.c8bb46b848ea4f65adf47640604ebeea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "07af11383a4eea32a1e3",
+    "url": "/site/static/css/main.9751ad75.chunk.css"
+  },
+  {
+    "revision": "07af11383a4eea32a1e3",
+    "url": "/site/static/js/main.07af1138.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/site/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/site/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "0837497f7abeff79305e",
+    "url": "/site/static/js/runtime~main.0837497f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/site/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "88fd6d6a3c0712a47e40894e5efb928d",
+    "url": "/site/index.html"
+  }
+];
